85f68ab29259834a2b4026043de62b96e311f0eb
Tim A <tabdiukov@gmail.com>
2021-05-17 16:17:09 +1000

    Upload my old but gold patch

---
+++ TAbdiukov_AlcatelContentManager_rmCopyrightRestrictions.py
@@ -0,0 +1,15 @@
+import binpatcher
+
+if __name__ == '__main__':
+	# Old: FF5010 3C01 0F85C9050000
+	# New: FF5010 3C01 909090909090
+
+	# Nopping the bad jump
+	
+	old = "(\\xFF\\x50\\x10)(\\x3C\\x01)(\\x0F\\x85\\xC9\\x05\\x00\\x00)"
+	# see https://stackoverflow.com/q/5984633
+	new = "\g<1>\g<2>\x90\x90\x90\x90\x90\x90"
+	
+	obj = binpatcher.Patcher(patch = "PTCH101|||ContentManager.exe|||" +old + "|||" + new + "|||0")
+	obj.payload()
+
