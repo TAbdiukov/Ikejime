ff89889f53d61b0c6dbf24170a587ded77f6d6a9
TA <tabdiukov@gmail.com>
2024-01-25 05:02:06 +0100

    basic support for queueing/commits

--- TAbdiukov_K10stat_anyCPUpatch.py
+++ TAbdiukov_K10stat_anyCPUpatch.py
@@ -1,130 +1,124 @@
 import re
 
-import binpatcher
+from binpatcher import *
 
 DEF_PREFIX = "PTCH101"
 BINFILE = "K10STAT.exe"
 DEF_FLAGS = "0"
 
 def yes_or_no(question):
-	answer = input(question + "(Y/N): ").lower().strip()
-	print("")
-	while not(answer == "y" or answer == "yes" or \
-	answer == "n" or answer == "no"):
-		print("Input yes or no")
-		answer = input(question + "(y/n):").lower().strip()
-		print("")
-	if answer[0] == "y":
-		return True
-	else:
-		return False
+    answer = input(question + "(Y/N): ").lower().strip()
+    print("")
+    while not(answer == "y" or answer == "yes" or \
+    answer == "n" or answer == "no"):
+        print("Input yes or no")
+        answer = input(question + "(y/n):").lower().strip()
+        print("")
+    if answer[0] == "y":
+        return True
+    else:
+        return False
 
 def gen_patchstring(old, new, prefix = DEF_PREFIX, binfile = BINFILE, flags = DEF_FLAGS):
-	s = "{0}|||{1}|||{2}|||{3}|||{4}".format(prefix, binfile, old, new, flags)
-	return s
+    s = "{0}|||{1}|||{2}|||{3}|||{4}".format(prefix, binfile, old, new, flags)
+    return s
 
 if __name__ == '__main__':
-	#	OllyDbg notes:
-	#	Crash procedure begins (w/o call) at 0040EA74
-	#	Procedure immediately pointed from: 
-	#	40E807 (1)	40E99B (1)	40E9E1 (1 and 4)	40EA6D (excluded)
-	#
-	#	Additional noteworthy jumps spotted:
-	#	40E920 -> 40EA68
-	#	
-	#	40EA68, jumps from:
-	#	40E8D3 (3.1)	40E920 (3.2)	40E92C (3.2)
-	#	40EA6F, jumps from:
-	#	40E860 (2)	40E86E (2)	40E897C (2)
-	#
-	#	Patches:
-	#	Part 1: 0040EA74 jumps / EB patches
-	#	1.1	40E807 / 40E7FE (Force unconditional jump)
-	#	Old:	85 C0 75 0A 68 DA 07 00 00 E9 68 02 00 00
-	#	New:	85 C0 EB 0A 68 DA 07 00 00 E9 68 02 00 00
-	#
-	#	1.2	40E99B / 40E992 (Force unconditional jump)
-	#	Old:	85 C0 75 0A 68 D3 07 00 00 E9 D4 00 00 00
-	#	New:	85 C0 EB 0A 68 D3 07 00 00 E9 D4 00 00 00
-	#
-	#	1.3	40E9E1 / 40E9DA (Force unconditional jump)
-	#	Old:	85 C0 75 0A 68 D5 07 00 00 E9 8E 00 00 00
-	#	New:	85 C0 EB 0A 68 D5 07 00 00 E9 8E 00 00 00
-	#
-	#	pointless to patch (part of pre-crash procedure, namely setting return integer)
-	#	0040EA66	0040EA6D (1)
-	#
-	#	Part 2: 40E8xx jumps / jumps to 40EA6F
-	#	0040E858, patch the cluster with 3 jumps to 40EA6F (NOP the jumps)
-	#	Old:	81 7C 24 3C 41 75 74 68 0F 85 09 02 00 00 81 7C 24 08 63 41 4D 44 0F 85 FB 01 00 00 81 7C 24 44 65 6E 74 69 0F 85 ED 01 00 00
-	#	New:	81 7C 24 3C 41 75 74 68 90 90 90 90 90 90 81 7C 24 08 63 41 4D 44 90 90 90 90 90 90 81 7C 24 44 65 6E 74 69 90 90 90 90 90 90
-	#
-	#	Part 3: jumps to 40EA68
-	#	3.1	0040E8CC, NOP one jump
-	#	Old:	[74 1B] 3D 00 0F 20 00 0F 85 8F 01 00 00 0F B6 C1 0D 00 11 00 00
-	#	New:	[74 1B] 3D 00 0F 20 00 90 90 90 90 90 90 0F B6 C1 0D 00 11 00 00
-	#	[74 1B] - likely to exclude (chose not to)
-	#	
-	#	3.2	0040E91B, patch the cluster with 2 jumps, NOP the jumps
-	#	Note: Patch definitely starts from 40E91B as it's jumped from 40E8C3
-	#	Old:	3D 00 0F 50 00 0F 85 42 01 00 00 F7 C1 00 00 0F 00 0F 85 36 01 00 00 0F B6 C1 0D 00 14 00 00
-	#	New:	3D 00 0F 50 00 90 90 90 90 90 90 F7 C1 00 00 0F 00 90 90 90 90 90 90 0F B6 C1 0D 00 14 00 00
-	#
-	#	Part 4: Checking for more dangerous branches / jumps / calls
-	#	> pointless to patch.. 0040EA66	0040EA6D
-	#	Thus checking from ~0040E932
-	#	*	Found forgotten 40E9DA jump - fixed
-	#	Checking jumps to push return int-s / final precrash instructions
-	#	*	push 7D2 - clean
-	#	*	push 7D1 - clean
-	#	*	0040EA74 - preemptive jumps set update
-	#	-	Found potentially interesting logic (will not be used):
-	#	0040EAA6: "Jump from 40E7CA" (jump over the whole check?)
-	#	Finally, 40EAB9 "RETN 10"
-	disclaimer = "The patch is offered as-is. By using this patch, you agree you are liable for the use of this patch and patched software, the author of the patch neither the author of K10Stat takes NO responsibility for the damages!! Do you agree? "
-	resp = yes_or_no(disclaimer)
-	if(resp == False):
-		print("OK, bye..")
-	elif(resp == True):
-		print("Loading data")
-		
-		patch_1_1_old = "\\x85\\xC0\\x75\\x0A\\x68\\xDA\\x07\\x00\\x00\\xE9\\x68\\x02\\x00\\x00"
-		patch_1_2_old = "\\x85\\xC0\\x75\\x0A\\x68\\xD3\\x07\\x00\\x00\\xE9\\xD4\\x00\\x00\\x00"
-		patch_1_3_old = "\\x85\\xC0\\x75\\x0A\\x68\\xD5\\x07\\x00\\x00\\xE9\\x8E\\x00\\x00\\x00"
-		
-		patch_2_old = "\\x81\\x7C\\x24\\x3C\\x41\\x75\\x74\\x68\\x0F\\x85\\x09\\x02\\x00\\x00\\x81\\x7C\\x24\\x08\\x63\\x41\\x4D\\x44\\x0F\\x85\\xFB\\x01\\x00\\x00\\x81\\x7C\\x24\\x44\\x65\\x6E\\x74\\x69\\x0F\\x85\\xED\\x01\\x00\\x00"
-		
-		patch_3_1_old = "\\x74\\x1B\\x3D\\x00\\x0F\\x20\\x00\\x0F\\x85\\x8F\\x01\\x00\\x00\\x0F\\xB6\\xC1\\x0D\\x00\\x11\\x00\\x00"
-		patch_3_2_old = "\\x3D\\x00\\x0F\\x50\\x00\\x0F\\x85\\x42\\x01\\x00\\x00\\xF7\\xC1\\x00\\x00\\x0F\\x00\\x0F\\x85\\x36\\x01\\x00\\x00\\x0F\\xB6\\xC1\\x0D\\x00\\x14\\x00\\x00"
-		
-		patch_1_1_new = "\x85\xC0\xEB\x0A\x68\xDA\x07\x00\x00\xE9\x68\x02\x00\x00"
-		patch_1_2_new = "\x85\xC0\xEB\x0A\x68\xD3\x07\x00\x00\xE9\xD4\x00\x00\x00"
-		patch_1_3_new = "\x85\xC0\xEB\x0A\x68\xD5\x07\x00\x00\xE9\x8E\x00\x00\x00"
-		
-		patch_2_new = "\x81\x7C\x24\x3C\x41\x75\x74\x68\x90\x90\x90\x90\x90\x90\x81\x7C\x24\x08\x63\x41\x4D\x44\x90\x90\x90\x90\x90\x90\x81\x7C\x24\x44\x65\x6E\x74\x69\x90\x90\x90\x90\x90\x90"
-		
-		patch_3_1_new = "\x74\x1B\x3D\x00\x0F\x20\x00\x90\x90\x90\x90\x90\x90\x0F\xB6\xC1\x0D\x00\x11\x00\x00"
-		patch_3_2_new = "\x3D\x00\x0F\x50\x00\x90\x90\x90\x90\x90\x90\xF7\xC1\x00\x00\x0F\x00\x90\x90\x90\x90\x90\x90\x0F\xB6\xC1\x0D\x00\x14\x00\x00"
-		
-		print("Begin patching..")
-		obj = binpatcher.Patcher(gen_patchstring(patch_1_1_old, patch_1_1_new))
-		obj.payload()
-		
-		obj = binpatcher.Patcher(gen_patchstring(patch_1_2_old, patch_1_2_new))
-		obj.payload()
+    #   OllyDbg notes:
+    #   Crash procedure begins (w/o call) at 0040EA74
+    #   Procedure immediately pointed from: 
+    #   40E807 (1)  40E99B (1)  40E9E1 (1 and 4)    40EA6D (excluded)
+    #
+    #   Additional noteworthy jumps spotted:
+    #   40E920 -> 40EA68
+    #   
+    #   40EA68, jumps from:
+    #   40E8D3 (3.1)    40E920 (3.2)    40E92C (3.2)
+    #   40EA6F, jumps from:
+    #   40E860 (2)  40E86E (2)  40E897C (2)
+    #
+    #   Patches:
+    #   Part 1: 0040EA74 jumps / EB patches
+    #   1.1 40E807 / 40E7FE (Force unconditional jump)
+    #   Old:    85 C0 75 0A 68 DA 07 00 00 E9 68 02 00 00
+    #   New:    85 C0 EB 0A 68 DA 07 00 00 E9 68 02 00 00
+    #
+    #   1.2 40E99B / 40E992 (Force unconditional jump)
+    #   Old:    85 C0 75 0A 68 D3 07 00 00 E9 D4 00 00 00
+    #   New:    85 C0 EB 0A 68 D3 07 00 00 E9 D4 00 00 00
+    #
+    #   1.3 40E9E1 / 40E9DA (Force unconditional jump)
+    #   Old:    85 C0 75 0A 68 D5 07 00 00 E9 8E 00 00 00
+    #   New:    85 C0 EB 0A 68 D5 07 00 00 E9 8E 00 00 00
+    #
+    #   pointless to patch (part of pre-crash procedure, namely setting return integer)
+    #   0040EA66    0040EA6D (1)
+    #
+    #   Part 2: 40E8xx jumps / jumps to 40EA6F
+    #   0040E858, patch the cluster with 3 jumps to 40EA6F (NOP the jumps)
+    #   Old:    81 7C 24 3C 41 75 74 68 0F 85 09 02 00 00 81 7C 24 08 63 41 4D 44 0F 85 FB 01 00 00 81 7C 24 44 65 6E 74 69 0F 85 ED 01 00 00
+    #   New:    81 7C 24 3C 41 75 74 68 90 90 90 90 90 90 81 7C 24 08 63 41 4D 44 90 90 90 90 90 90 81 7C 24 44 65 6E 74 69 90 90 90 90 90 90
+    #
+    #   Part 3: jumps to 40EA68
+    #   3.1 0040E8CC, NOP one jump
+    #   Old:    [74 1B] 3D 00 0F 20 00 0F 85 8F 01 00 00 0F B6 C1 0D 00 11 00 00
+    #   New:    [74 1B] 3D 00 0F 20 00 90 90 90 90 90 90 0F B6 C1 0D 00 11 00 00
+    #   [74 1B] - likely to exclude (chose not to)
+    #   
+    #   3.2 0040E91B, patch the cluster with 2 jumps, NOP the jumps
+    #   Note: Patch definitely starts from 40E91B as it's jumped from 40E8C3
+    #   Old:    3D 00 0F 50 00 0F 85 42 01 00 00 F7 C1 00 00 0F 00 0F 85 36 01 00 00 0F B6 C1 0D 00 14 00 00
+    #   New:    3D 00 0F 50 00 90 90 90 90 90 90 F7 C1 00 00 0F 00 90 90 90 90 90 90 0F B6 C1 0D 00 14 00 00
+    #
+    #   Part 4: Checking for more dangerous branches / jumps / calls
+    #   > pointless to patch.. 0040EA66 0040EA6D
+    #   Thus checking from ~0040E932
+    #   *   Found forgotten 40E9DA jump - fixed
+    #   Checking jumps to push return int-s / final precrash instructions
+    #   *   push 7D2 - clean
+    #   *   push 7D1 - clean
+    #   *   0040EA74 - preemptive jumps set update
+    #   -   Found potentially interesting logic (will not be used):
+    #   0040EAA6: "Jump from 40E7CA" (jump over the whole check?)
+    #   Finally, 40EAB9 "RETN 10"
+    disclaimer = "The patch is offered as-is. By using this patch, you agree you are liable for the use of this patch and patched software, the author of the patch neither the author of K10Stat takes NO responsibility for the damages!! Do you agree? "
+    resp = yes_or_no(disclaimer)
+    if(resp == False):
+        print("OK, bye..")
+    elif(resp == True):
+        print("Loading data")
+        
+        patch_1_1_old = "\\x85\\xC0\\x75\\x0A\\x68\\xDA\\x07\\x00\\x00\\xE9\\x68\\x02\\x00\\x00"
+        patch_1_2_old = "\\x85\\xC0\\x75\\x0A\\x68\\xD3\\x07\\x00\\x00\\xE9\\xD4\\x00\\x00\\x00"
+        patch_1_3_old = "\\x85\\xC0\\x75\\x0A\\x68\\xD5\\x07\\x00\\x00\\xE9\\x8E\\x00\\x00\\x00"
+        
+        patch_2_old = "\\x81\\x7C\\x24\\x3C\\x41\\x75\\x74\\x68\\x0F\\x85\\x09\\x02\\x00\\x00\\x81\\x7C\\x24\\x08\\x63\\x41\\x4D\\x44\\x0F\\x85\\xFB\\x01\\x00\\x00\\x81\\x7C\\x24\\x44\\x65\\x6E\\x74\\x69\\x0F\\x85\\xED\\x01\\x00\\x00"
+        
+        patch_3_1_old = "\\x74\\x1B\\x3D\\x00\\x0F\\x20\\x00\\x0F\\x85\\x8F\\x01\\x00\\x00\\x0F\\xB6\\xC1\\x0D\\x00\\x11\\x00\\x00"
+        patch_3_2_old = "\\x3D\\x00\\x0F\\x50\\x00\\x0F\\x85\\x42\\x01\\x00\\x00\\xF7\\xC1\\x00\\x00\\x0F\\x00\\x0F\\x85\\x36\\x01\\x00\\x00\\x0F\\xB6\\xC1\\x0D\\x00\\x14\\x00\\x00"
+        
+        patch_1_1_new = "\x85\xC0\xEB\x0A\x68\xDA\x07\x00\x00\xE9\x68\x02\x00\x00"
+        patch_1_2_new = "\x85\xC0\xEB\x0A\x68\xD3\x07\x00\x00\xE9\xD4\x00\x00\x00"
+        patch_1_3_new = "\x85\xC0\xEB\x0A\x68\xD5\x07\x00\x00\xE9\x8E\x00\x00\x00"
+        
+        patch_2_new = "\x81\x7C\x24\x3C\x41\x75\x74\x68\x90\x90\x90\x90\x90\x90\x81\x7C\x24\x08\x63\x41\x4D\x44\x90\x90\x90\x90\x90\x90\x81\x7C\x24\x44\x65\x6E\x74\x69\x90\x90\x90\x90\x90\x90"
+        
+        patch_3_1_new = "\x74\x1B\x3D\x00\x0F\x20\x00\x90\x90\x90\x90\x90\x90\x0F\xB6\xC1\x0D\x00\x11\x00\x00"
+        patch_3_2_new = "\x3D\x00\x0F\x50\x00\x90\x90\x90\x90\x90\x90\xF7\xC1\x00\x00\x0F\x00\x90\x90\x90\x90\x90\x90\x0F\xB6\xC1\x0D\x00\x14\x00\x00"
+        
+        print("Loading commits..")
+        queue = Commits()
+        
+        queue.commit(Patcher(Cook(patch_1_1_old, patch_1_1_new, file="K10STAT.exe")))
+        queue.commit(Patcher(Cook(patch_1_2_old, patch_1_2_new, file="K10STAT.exe")))
+        
+        queue.commit(Patcher(Cook(patch_2_old, patch_2_new, file="K10STAT.exe")))
 
-		obj = binpatcher.Patcher(gen_patchstring(patch_2_old, patch_2_new))
-		obj.payload()
+        queue.commit(Patcher(Cook(patch_3_1_old, patch_3_1_new, file="K10STAT.exe")))
+        queue.commit(Patcher(Cook(patch_3_2_old, patch_3_2_new, file="K10STAT.exe")))
+        
+        print("Pushing..")
+        
+        queue.push()
 
-		obj = binpatcher.Patcher(gen_patchstring(patch_3_1_old, patch_3_1_new))
-		obj.payload()
-
-		obj = binpatcher.Patcher(gen_patchstring(patch_3_2_old, patch_3_2_new))
-		obj.payload()
-		print("Done.")
-		
-		
-
-
-	
-	
+        print("Done.")
--- binpatcher.py
+++ binpatcher.py
@@ -20,264 +20,267 @@ logger.setLevel(logging.DEBUG)
 
 
 class Cook:
-    DEF_PREFIX = 'PTCH101'
-    FILE = 'default_binfile.exe'
-    DEF_FLAGS = '0'
+	DEF_PREFIX = 'PTCH101'
+	FILE = 'default_binfile.exe'
+	DEF_FLAGS = '0'
 
-    def __init__(self, old, new, prefix = DEF_PREFIX, file = FILE, flags = DEF_FLAGS):
-        self.old = old
-        self.new = new
-        self.prefix = prefix
-        self.file = file
-        self.flags = flags
+	def __init__(self, old, new, prefix = DEF_PREFIX, file = FILE, flags = DEF_FLAGS):
+		self.old = old
+		self.new = new
+		self.prefix = prefix
+		self.file = file
+		self.flags = flags
 
-    def cookBasic(self):
-        s = "{0}|||{1}|||{2}|||{3}|||{4}".format(self.prefix, self.file, self.old, self.new, self.flags)
-        return s
-
+	def cookBasic(self):
+		s = "{0}|||{1}|||{2}|||{3}|||{4}".format(self.prefix, self.file, self.old, self.new, self.flags)
+		return s
+		
+	def __str__(self):
+		return self.cookBasic()
 
 class Commits:
-    def __init__(self):
-        self.commits = []
+	def __init__(self):
+		self.commits = []
+		
 
-    def commit(self, c):
-        if isinstance(c, Patcher):
-            self.commits.append(c)
+	def commit(self, c):
+		if isinstance(c, Patcher):
+			self.commits.append(c)
 
-    def push(self):
-        for commit in self.commits:
-            commit.payload()
+	def push(self):
+		for commit in self.commits:
+			commit.payload()
 
 
 class Patcher:
-    DELIM = "|||"
-    HELP = """{0} - patches {1}
+	DELIM = "|||"
+	HELP = """{0} - patches {1}
 Usage:
 {0} (when in software directory) OR
 {0} [directory] OR
 {0} [directory]/{1}
 """
 
-    def __init__(self, patch = "", argv = [], kwargs = {}):
-        for k,v in kwargs.items():
-            setattr(self, k, v)
-        
-        self.argv = argv
-        self.patch = patch
-        
-        # important data
-        self.target = "(unnamed_target).exe"
-        self.src = None
-        self.dst = None
-        
-        # stream
-        self.full_fname = None
-        self.dir = None
-        self._cnt = 0
-        
-        self.hash_old = None
-        self.hash_new = None
-        
-        # assert the same length for both 
-        self.flag_samelen = True
-        
-        # secondary
-        self.tool_name = None
-    
-    def interpret_input(self):
-        try:
-            self.tool_name = self.argv[0]
-        except: 
-            self.tool_name = "(patcher)"
-        user_argv = self.argv[1:]
-        patch = self.patch
-        
-        # if argv passed
-        if(len(user_argv)):
-            first = user_argv[0]
-            # if first argv is HELP
-            if(first.lower() == "help"):
-                return False
-            else:
-                buf = first
-                for s in user_argv[1:]:
-                    buf = buf + " " + s
-                # overwrite patch
-                self.patch = buf
+	def __init__(self, patch = "", argv = [], kwargs = {}):
+		for k,v in kwargs.items():
+			setattr(self, k, v)
+		
+		self.argv = argv
+		self.patch = patch
+		
+		# important data
+		self.target = "(unnamed_target).exe"
+		self.src = None
+		self.dst = None
+		
+		# stream
+		self.full_fname = None
+		self.dir = None
+		self._cnt = 0
+		
+		self.hash_old = None
+		self.hash_new = None
+		
+		# assert the same length for both 
+		self.flag_samelen = True
+		
+		# secondary
+		self.tool_name = None
+	
+	def interpret_input(self):
+		try:
+			self.tool_name = self.argv[0]
+		except: 
+			self.tool_name = "(patcher)"
+		user_argv = self.argv[1:]
+		patch = self.patch
+		
+		# if argv passed
+		if(len(user_argv)):
+			first = user_argv[0]
+			# if first argv is HELP
+			if(first.lower() == "help"):
+				return False
+			else:
+				buf = first
+				for s in user_argv[1:]:
+					buf = buf + " " + s
+				# overwrite patch
+				self.patch = buf
 
-                return True
-        else:
-            if(len(patch)):
-                return True
-            # data entered through side channels
-            elif(self.is_target_acquired()):
-                return True
-            return False
-                    
-    
-    def patch_unpack(self):
-        s = self.patch
+				return True
+		else:
+			if(patch is not None):
+				return True
+			# data entered through side channels
+			elif(self.is_target_acquired()):
+				return True
+			return False
+					
+	
+	def patch_unpack(self):
+		s = self.patch
 
-        d = self.DELIM
-        
-        soup = s.split(d)
-        soup_cnt = len(soup)
-        # VISION:
-        # "PTCHv1.25|||Game.exe|||b'PC_AI'|||b'\x00\x00\x00\x00\x00'|||(flags)"
-        assert(soup_cnt >= 4)
-        # required data
-        self.reserved = soup[0]
-        self.target = soup[1]
-        
-        """
-        self.src = bytes(soup[2], "raw_unicode_escape")
-        self.dst = bytes(soup[3], "raw_unicode_escape")     
-        """
-        
-        self.src = bytes(soup[2], "raw_unicode_escape")
-        self.dst = bytes(soup[3], "raw_unicode_escape")     
-        
-        if(soup_cnt >= 5): #flags
-            flags = soup[4]
-            self.flag_samelen = int(flags[0])
-     
-    # technical
-    @property
-    def cnt(self):
-        return self._cnt
-    
-    @cnt.setter
-    def cnt(self, k):
-        self._cnt = k
-    
-    def inc_cnt(self):
-        self.cnt = self.cnt + 1
+		d = self.DELIM
+		
+		soup = s.__str__().split(d)
+		soup_cnt = len(soup)
+		# VISION:
+		# "PTCHv1.25|||Game.exe|||b'PC_AI'|||b'\x00\x00\x00\x00\x00'|||(flags)"
+		assert(soup_cnt >= 4)
+		# required data
+		self.reserved = soup[0]
+		self.target = soup[1]
+		
+		"""
+		self.src = bytes(soup[2], "raw_unicode_escape")
+		self.dst = bytes(soup[3], "raw_unicode_escape")     
+		"""
+		
+		self.src = bytes(soup[2], "raw_unicode_escape")
+		self.dst = bytes(soup[3], "raw_unicode_escape")     
+		
+		if(soup_cnt >= 5): #flags
+			flags = soup[4]
+			self.flag_samelen = int(flags[0])
+	 
+	# technical
+	@property
+	def cnt(self):
+		return self._cnt
+	
+	@cnt.setter
+	def cnt(self, k):
+		self._cnt = k
+	
+	def inc_cnt(self):
+		self.cnt = self.cnt + 1
 
-    def clr_cnt(self):
-        self.cnt = 0
+	def clr_cnt(self):
+		self.cnt = 0
 
-    def find_target(self, guess = ""):
-        std_full_fname = self.target
-        #logger.info("Main guess: "+str(guess))
-        # Try guess first
-        if(len(guess)):
-            path_guess = Path(guess)
-            if(path_guess.is_file()):
-                self.full_fname = str(guess)
-                self.dir = str(path_guess.parent)
-                return ;
-            elif(path_guess.is_dir()):
-                guess_new = "{}/{}".format(str(guess), std_full_fname)
-                path_guess_new = Path(guess_new)
-                if(path_guess_new.is_file()):
-                    self.full_fname = guess_new
-                    self.dir = str(path_guess_new.parent)
-                    return ;
-                    
-        # then try in local dir
-        path_local = Path.cwd() / Path(std_full_fname)
-        logger.info("Guess 2: "+str(path_local))
-        logger.info("Guess 2 local: "+str(path_local.parent))
+	def find_target(self, guess = ""):
+		std_full_fname = self.target
+		#logger.info("Main guess: "+str(guess))
+		# Try guess first
+		if(len(guess)):
+			path_guess = Path(guess)
+			if(path_guess.is_file()):
+				self.full_fname = str(guess)
+				self.dir = str(path_guess.parent)
+				return ;
+			elif(path_guess.is_dir()):
+				guess_new = "{}/{}".format(str(guess), std_full_fname)
+				path_guess_new = Path(guess_new)
+				if(path_guess_new.is_file()):
+					self.full_fname = guess_new
+					self.dir = str(path_guess_new.parent)
+					return ;
+					
+		# then try in local dir
+		path_local = Path.cwd() / Path(std_full_fname)
+		logger.info("Guess 2: "+str(path_local))
+		logger.info("Guess 2 local: "+str(path_local.parent))
 
-        if(path_local.is_file()):
-            self.full_fname = std_full_fname
-            self.dir = str(path_local.parent)
-            return ;
-        else:
-            logger.info("Target file not found")
-            assert(0)
-        
-        
-    @staticmethod
-    def hash_pretty(k):
-        return (hex(k).split("x")[-1].upper())
-            
-        # then try in local dir
-        path_local = Path(std_full_fname).resolve()
-        if(path_local.is_file()):
-            self.full_fname = std_full_fname
-            self.dir = str(path_local.parent)
-        
-    def is_target_acquired(self):
-        # self.dst not needed
-        return ((len(self.dir) > 0) and (len(self.full_fname) > 0) and (len(self.src) > 0))
-        
-    def help_fillin(self):
-        return self.HELP.format(self.tool_name, self.target)
-    
-    # To be deprecated
-    def replace_hook(self):
-        logger.info("replace")
-        self.inc_cnt()
-        return self.dst
-        
-    def perform_patch(self, flag_samelen = True):   
-        if(self.flag_samelen):
-            assert(len(self.src) == len(self.dst))
-            
-        # valid patch chk
-        assert(len(str(self.src)) > 0)
-    
-        fp = open(self.full_fname, "rb")
-        binary = fp.read()
-        self.hash_old = zlib.adler32(binary) 
-        fp.close()
-        assert(len(binary))
-        
-        self.clr_cnt()
-        #logger.info(self.src)
-        src_compiled = re.compile(self.src, flags = re.DOTALL)
-        
-        self.cnt = -1
-        patched,self.cnt = re.subn(src_compiled, repl=self.dst, string=binary)        
+		if(path_local.is_file()):
+			self.full_fname = std_full_fname
+			self.dir = str(path_local.parent)
+			return ;
+		else:
+			logger.info("Target file not found")
+			assert(0)
+		
+		
+	@staticmethod
+	def hash_pretty(k):
+		return (hex(k).split("x")[-1].upper())
+			
+		# then try in local dir
+		path_local = Path(std_full_fname).resolve()
+		if(path_local.is_file()):
+			self.full_fname = std_full_fname
+			self.dir = str(path_local.parent)
+		
+	def is_target_acquired(self):
+		# self.dst not needed
+		return ((len(self.dir) > 0) and (len(self.full_fname) > 0) and (len(self.src) > 0))
+		
+	def help_fillin(self):
+		return self.HELP.format(self.tool_name, self.target)
+	
+	# To be deprecated
+	def replace_hook(self):
+		logger.info("replace")
+		self.inc_cnt()
+		return self.dst
+		
+	def perform_patch(self, flag_samelen = True):   
+		if(self.flag_samelen):
+			assert(len(self.src) == len(self.dst))
+			
+		# valid patch chk
+		assert(len(str(self.src)) > 0)
+	
+		fp = open(self.full_fname, "rb")
+		binary = fp.read()
+		self.hash_old = zlib.adler32(binary) 
+		fp.close()
+		assert(len(binary))
+		
+		self.clr_cnt()
+		#logger.info(self.src)
+		src_compiled = re.compile(self.src, flags = re.DOTALL)
+		
+		self.cnt = -1
+		patched,self.cnt = re.subn(src_compiled, repl=self.dst, string=binary)        
 
-        self.hash_new = zlib.adler32(patched) 
+		self.hash_new = zlib.adler32(patched) 
 
-        # save results
-        fp = open(self.full_fname, "wb")
-        fp.write(patched)
-        fp.close()
-        
-    def copy_orig(self, suffix = ".orig"):
-        src = self.full_fname
-        dst = self.full_fname + suffix
-        
-        copyfile(src, dst)
-        return dst
-        
-    def payload(self, width = 40):
-        valid = self.interpret_input()
-    
-        logger.info("="*width)
-        if(not valid):
-            logger.info(self.help_fillin())
-        else:
-            self.patch_unpack()
-            logger.info("Patch tgt: "+str(self.target))
-            logger.info("Patch src: "+str(self.src))
-            logger.info("Patch dst: "+str(self.dst))
-            logger.info("")
+		# save results
+		fp = open(self.full_fname, "wb")
+		fp.write(patched)
+		fp.close()
+		
+	def copy_orig(self, suffix = ".orig"):
+		src = self.full_fname
+		dst = self.full_fname + suffix
+		
+		copyfile(src, dst)
+		return dst
+		
+	def payload(self, width = 40):
+		valid = self.interpret_input()
+	
+		logger.info("="*width)
+		if(not valid):
+			logger.info(self.help_fillin())
+		else:
+			self.patch_unpack()
+			logger.info("Patch tgt: "+str(self.target))
+			logger.info("Patch src: "+str(self.src))
+			logger.info("Patch dst: "+str(self.dst))
+			logger.info("")
 
-            self.find_target(guess = self.target)
+			self.find_target(guess = self.target)
 
-            assert(self.is_target_acquired)
-            buf = self.copy_orig()
-            logger.info("Backup saved to: "+buf)
-            self.perform_patch()
-            logger.info("* Matches: "+str(self.cnt))
-            
-            old = self.hash_pretty(self.hash_old)
-            new = self.hash_pretty(self.hash_new)
-            
-            logger.info("* Old hash: "+old)
-            logger.info("* New hash: "+new)
-            if(self.cnt > 0):
-                logger.info("Patched successfully!")
-            else:
-                logger.info("Patching failed. Is patch incompatible?")
-        
-            logger.info("="*width)
-            
+			assert(self.is_target_acquired)
+			buf = self.copy_orig()
+			logger.info("Backup saved to: "+buf)
+			self.perform_patch()
+			logger.info("* Matches: "+str(self.cnt))
+			
+			old = self.hash_pretty(self.hash_old)
+			new = self.hash_pretty(self.hash_new)
+			
+			logger.info("* Old hash: "+old)
+			logger.info("* New hash: "+new)
+			if(self.cnt > 0):
+				logger.info("Patched successfully!")
+			else:
+				logger.info("Patching failed. Is patch incompatible?")
+		
+			logger.info("="*width)
+			
 if __name__ == '__main__':
-    obj = Patcher(argv = sys.argv)
-    obj.payload()
+	obj = Patcher(argv = sys.argv)
+	obj.payload()
